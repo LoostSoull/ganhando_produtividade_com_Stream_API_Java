132ba4fb94d6f3a3217db90a26f3ebef8bc7ae12
lucas silva <lucas.silva.cardoso1996@gmail.com>
2023-09-15 14:14:10 -0300

    concluido desafios 9

--- src/main/java/DesafiosStream/Desafio_9.java
+++ src/main/java/DesafiosStream/Desafio_9.java
@@ -1,4 +1,24 @@
 package DesafiosStream;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Desafio_9 {
+
+    public static void main(String[] args) {
+        //Verificar se todos os números da lista são distintos (não se repetem)
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+      List<Integer> numerosDiferentes = numeros.stream().distinct().toList();
+
+      boolean distintos = numeros.size() == numerosDiferentes.size();
+
+        System.out.println("Os numeros sao distintos? : " + distintos);
+
+
+
+    }
+
 }
